70d5e8fa37274bd106e4d952ac275e9b38f526c2
Fabricio Gabriel <floyd9732@gmail.com>
2025-01-30 00:20:07 -0300

    Re-implementing type defs and resolvers

--- src/graphql/typeDefs.ts
+++ src/graphql/typeDefs.ts
@@ -1,84 +1,156 @@
-import { gql } from "graphql-tag";
-  
+import { gql } from 'graphql-tag';
+
 const typeDefs = gql`
-  # User Model
+  scalar DateTime
+
   type User {
     id: ID!
     name: String
-    email: String
-    emailVerified: String
+    email: String!
+    emailVerified: DateTime
     image: String
-    createdAt: String
-    updatedAt: String
-    posts: [Post!]!
     accounts: [Account!]!
     sessions: [Session!]!
+    authenticators: [Authenticator!]!
+    posts: [Post!]!
+    likes: [Like!]!
+    comments: [Comment!]!
+    createdAt: DateTime!
+    updatedAt: DateTime!
   }
 
-  # Post Model
+  type Account {
+    userId: String!
+    type: String!
+    provider: String!
+    providerAccountId: String!
+    refresh_token: String
+    access_token: String
+    expires_at: Int
+    token_type: String
+    scope: String
+    id_token: String
+    session_state: String
+    createdAt: DateTime!
+    updatedAt: DateTime!
+    user: User!
+  }
+
+  type Session {
+    sessionToken: String!
+    userId: String!
+    expires: DateTime!
+    createdAt: DateTime!
+    updatedAt: DateTime!
+    user: User!
+  }
+
+  type VerificationToken {
+    identifier: String!
+    token: String!
+    expires: DateTime!
+  }
+
+  type Authenticator {
+    credentialID: String!
+    userId: String!
+    providerAccountId: String!
+    credentialPublicKey: String!
+    counter: Int!
+    credentialDeviceType: String!
+    credentialBackedUp: Boolean!
+    transports: String
+    createdAt: DateTime!
+    updatedAt: DateTime!
+    user: User!
+  }
+
   type Post {
     id: ID!
     title: String!
-    content: String
-    published: Boolean!
-    createdAt: String!
-    updatedAt: String!
-    author: User
+    content: String!
+    thumbnail: String
+    authorId: String!
+    author: User!
+    likes: [Like!]!
+    comments: [Comment!]!
+    categories: [Category!]!
+    createdAt: DateTime!
+    updatedAt: DateTime!
   }
 
-  # Account Model
-  type Account {
+  type Category {
     id: ID!
-    providerType: String!
-    providerId: String!
-    providerAccountId: String!
-    refreshToken: String
-    accessToken: String
-    accessTokenExpires: String
-    createdAt: String!
-    updatedAt: String!
-    user: User!
-  }
-
-  # Session Model
-  type Session {
-    id: ID!
-    sessionToken: String!
-    accessToken: String!
-    expires: String!
-    createdAt: String!
-    updatedAt: String!
-    user: User!
-  }
-
-  # VerificationRequest Model
-  type VerificationRequest {
-    id: ID!
-    identifier: String!
-    token: String!
-    expires: String!
-    createdAt: String!
-    updatedAt: String!
-  }
-
-  # Queries
-  type Query {
-    users: [User!]!
-    user(id: String!): User
+    name: String!
     posts: [Post!]!
-    post(id: ID!): Post
-    sessions: [Session!]!
-    session(id: String!): Session
-    verificationRequests: [VerificationRequest!]!
-    verificationRequest(id: String!): VerificationRequest
   }
 
-  # Mutations
+  type Like {
+    id: ID!
+    userId: String!
+    postId: String!
+    user: User!
+    post: Post!
+    createdAt: DateTime!
+  }
+
+  type Comment {
+    id: ID!
+    content: String!
+    userId: String!
+    postId: String!
+    user: User!
+    post: Post!
+    createdAt: DateTime!
+    updatedAt: DateTime!
+  }
+
+  type Query {
+    # User Queries
+    user(id: ID!): User
+    users: [User!]!
+
+    # Post Queries
+    post(id: ID!): Post
+    posts: [Post!]!
+
+    # Category Queries
+    category(id: ID!): Category
+    categories: [Category!]!
+
+    # Like Queries
+    like(id: ID!): Like
+    likes: [Like!]!
+
+    # Comment Queries
+    comment(id: ID!): Comment
+    comments: [Comment!]!
+  }
+
   type Mutation {
-    createUser(name: String, email: String): User!
-    createPost(title: String!, content: String, authorId: String): Post!
-    updatePost(id: ID!, title: String, content: String, published: Boolean): Post
-    deletePost(id: ID!): Post
+    # User Mutations
+    createUser(name: String, email: String!, image: String): User!
+    updateUser(id: ID!, name: String, email: String, image: String): User!
+    deleteUser(id: ID!): User!
+
+    # Post Mutations
+    createPost(title: String!, content: String!, authorId: String!, thumbnail: String): Post!
+    updatePost(id: ID!, title: String, content: String, thumbnail: String): Post!
+    deletePost(id: ID!): Post!
+
+    # Category Mutations
+    createCategory(name: String!): Category!
+    updateCategory(id: ID!, name: String!): Category!
+    deleteCategory(id: ID!): Category!
+
+    # Like Mutations
+    createLike(userId: String!, postId: String!): Like!
+    deleteLike(id: ID!): Like!
+
+    # Comment Mutations
+    createComment(content: String!, userId: String!, postId: String!): Comment!
+    updateComment(id: ID!, content: String!): Comment!
+    deleteComment(id: ID!): Comment!
   }
 `;
 

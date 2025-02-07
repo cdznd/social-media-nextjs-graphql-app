f61aff8001f63e64a17e4b97a004c15745a0bc6d
Fabricio Gabriel <floyd9732@gmail.com>
2025-02-07 00:32:30 -0300

    small clean up on feed

--- src/app/app/page.tsx
+++ src/app/app/page.tsx
@@ -4,44 +4,43 @@ import { GET_FEED_POSTS } from "@/lib/graphql/mutations/mutations";
 import { auth } from "@/lib/auth";
 import { Container } from "@mui/system";
 
-async function getFeedData(userId: String, searchString?: string, category?: string) {
+async function getFeedData(userId: string, searchString?: string, category?: string) {
   const apolloClient = createApolloClient();
   try {
     const { data } = await apolloClient.query({
       query: GET_FEED_POSTS,
       variables: {
         userId,
+        searchString,
+        category
       },
     });
     return { data, feedError: null };
   } catch (error) {
-
-    console.log(JSON.stringify(error))
-
+    console.error(error)
     return { data: null, feedError: error };
   }
 }
 
 type SearchParamsProps = {
   searchParams: {
-    category?: string,
-    search?: string
+    search?: string,
+    category?: string
   }
 }
 
 export default async function Home(
-  { searchParams }: SearchParamsProps
+  { searchParams: { search, category }}: SearchParamsProps
 ) {
   const session = await auth()
-  
+  // TODO: better handle the feed Error here
   const { data, feedError } = await getFeedData(
     session?.user?.id!,
-    searchParams?.search,
-    undefined
+    search,
+    category
   );
-
-  const feedPosts = data?.feedPosts
-
+  // TODO: Add a type for the feedPosts here, like feedPosts: type
+  const feedPosts = data?.feedPosts ?? []
   return (
     <Container>
       <Feed feedData={feedPosts} />

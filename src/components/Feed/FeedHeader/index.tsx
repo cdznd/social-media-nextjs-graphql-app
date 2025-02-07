2e516e8b5f6492e223ca72cc092ff9e2eaca02fa
Fabricio Gabriel <floyd9732@gmail.com>
2025-02-07 20:24:23 -0300

    Update on feed header to be a server component

--- src/components/Feed/FeedHeader/index.tsx
+++ src/components/Feed/FeedHeader/index.tsx
@@ -1,94 +1,58 @@
-"use client"
-import { Box, Chip } from "@mui/material"
-import Search from "@/components/Search";
+import { Box } from "@mui/material";
+import createApolloClient from "@/lib/apolloClient";
+import { GET_CATEGORIES } from "@/lib/graphql/queries/category";
+import Search from "./Search";
+import CategorySelector from "./CategorySelector";
 
-import { useRouter, useSearchParams } from "next/navigation";
+async function getCategoriesData() {
+    const apolloClient = createApolloClient();
+    try {
+        const { data } = await apolloClient.query({
+            query: GET_CATEGORIES,
+        });
+        return { data, feedError: null };
+    } catch (error) {
+        console.error(error)
+        return { data: null, feedError: error };
+    }
+}
 
-export default function FeedHeader() {
-
-    const handleClick = () => {
-        console.info('You clicked the filter chip.');
-    };
-    
-    const router = useRouter();
-    const searchParams = useSearchParams();
-    
-    const handleCategoryClick = (category: string) => {
-        const params = new URLSearchParams(searchParams.toString());
-        params.set("category", category);
-        router.push(`?${params.toString()}`, { scroll: false });
-    };
-
+export default async function FeedHeader() {
+    const { data } = await getCategoriesData();
     return (
         <Box
             sx={{
-                display: 'flex',
-                flexDirection: { xs: 'column-reverse', md: 'row' },
-                width: '100%',
-                justifyContent: 'space-between',
-                alignItems: { xs: 'start', md: 'center' },
+                display: "flex",
+                flexDirection: { xs: "column-reverse", md: "row" },
+                width: "100%",
+                justifyContent: "space-between",
+                alignItems: { xs: "start", md: "center" },
                 gap: 4,
-                overflow: 'auto',
+                overflow: "auto",
             }}
         >
             <Box
                 sx={{
-                    display: 'inline-flex',
-                    flexDirection: 'row',
+                    display: "inline-flex",
+                    flexDirection: "row",
                     gap: 3,
-                    overflow: 'auto',
+                    overflow: "auto",
                 }}
             >
-                <Chip onClick={handleClick} size="medium" label="All categories" />
-                <Chip
-                    onClick={() => handleCategoryClick('company')}
-                    size="medium"
-                    label="Company"
-                    sx={{
-                        backgroundColor: 'transparent',
-                        border: 'none',
-                    }}
-                />
-                <Chip
-                    onClick={handleClick}
-                    size="medium"
-                    label="Product"
-                    sx={{
-                        backgroundColor: 'transparent',
-                        border: 'none',
-                    }}
-                />
-                <Chip
-                    onClick={handleClick}
-                    size="medium"
-                    label="Design"
-                    sx={{
-                        backgroundColor: 'transparent',
-                        border: 'none',
-                    }}
-                />
-                <Chip
-                    onClick={handleClick}
-                    size="medium"
-                    label="Engineering"
-                    sx={{
-                        backgroundColor: 'transparent',
-                        border: 'none',
-                    }}
-                />
+                <CategorySelector
+                    categories={data.categories} />
             </Box>
             <Box
                 sx={{
-                    display: { xs: 'none', sm: 'flex' },
-                    flexDirection: 'row',
+                    display: { xs: "none", sm: "flex" },
+                    flexDirection: "row",
                     gap: 1,
-                    width: { xs: '100%', md: 'fit-content' },
-                    overflow: 'auto',
+                    width: { xs: "100%", md: "fit-content" },
+                    overflow: "auto",
                 }}
             >
                 <Search />
             </Box>
         </Box>
-    )
+    );
 }
-

86141572d8275590d6876ddf911bba84338e3b7d
Fabricio Gabriel <floyd9732@gmail.com>
2025-02-08 22:27:04 -0300

    Fixing mutations on new post page

--- src/app/app/new-post/page.tsx
+++ src/app/app/new-post/page.tsx
@@ -16,7 +16,7 @@ import {
     Checkbox
 } from "@mui/material";
 import { useMutation } from "@apollo/client";
-import { CREATE_POST_MUTATION } from "@/lib/graphql/mutations/mutations";
+import { CREATE_POST_MUTATION } from "@/lib/graphql/fragments/mutations/mutations";
 import { StyledTextarea } from "@/components/common/CustomTextArea";
 import ClearIcon from '@mui/icons-material/Clear';
 

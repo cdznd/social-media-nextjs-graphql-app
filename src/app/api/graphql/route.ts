139e4dea4e02134d4d59be58032731522afef601
Fabricio Gabriel <floyd9732@gmail.com>
2025-02-14 19:50:01 -0300

    Disablign graphql introspection on production and requiring auth to access it

--- src/app/api/graphql/route.ts
+++ src/app/api/graphql/route.ts
@@ -1,12 +1,17 @@
-
 import { NextRequest } from "next/server";
 import { startServerAndCreateNextHandler } from "@as-integrations/next";
+import { getToken } from "next-auth/jwt";
 import server from "@/lib/graphql/server";
-
 import { createContext, Context } from "@/lib/prisma/context";
 
 const handler = startServerAndCreateNextHandler<NextRequest, Context>(server, {
-  context: createContext,
+  context: async (req) => {
+    const token = await getToken({ req });
+    if (process.env.NODE_ENV === 'production' && !token) {
+      throw new Error('Authentication required');
+    }
+    return createContext();
+  },
 });
 
 export { handler as GET, handler as POST };

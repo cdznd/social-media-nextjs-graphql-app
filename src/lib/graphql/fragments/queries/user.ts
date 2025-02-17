a18b2aad600128711e8ea8a92ae74ff82a064daf
Fabricio Gabriel <floyd9732@gmail.com>
2025-02-17 20:55:12 -0300

    Updating get user profile query frag

--- src/lib/graphql/fragments/queries/user.ts
+++ src/lib/graphql/fragments/queries/user.ts
@@ -1,6 +1,16 @@
 import gql from "graphql-tag";
 
-export const GET_USER_PROFILE = gql`
+export const GET_ALL_USERS = gql`
+    query GetAllUsers {
+        users {
+            id
+            name
+            email
+        }
+    }
+`
+
+export const GET_MY_USER_PROFILE = gql`
     query GetUserProfile(
         $userId: String!
     ) {
@@ -45,3 +55,35 @@ export const GET_USER_PROFILE = gql`
         }
     }
 `
+
+export const GET_USER_PROFILE = gql`
+    query GetUserProfile(
+        $userId: String!
+    ) {
+        user(userId: $userId) {
+            email
+            name
+            image
+            friends {
+                user {
+                    id
+                    name
+                    email
+                }
+                status
+            }
+            posts {
+                id
+                title
+                thumbnail
+                authorId
+                createdAt
+                updatedAt
+                author {
+                    id
+                    name
+                }
+            }
+        }
+    }
+` 

1844bd7b49d15d2e40dbbb1f11ca97fac79b1dcc
Fabricio Gabriel <floyd9732@gmail.com>
2025-02-19 15:01:38 -0300

    Adding enums type for notification

--- src/lib/graphql/enums/common.ts
+++ src/lib/graphql/enums/common.ts
@@ -9,3 +9,13 @@ export const FriendshipStatus = enumType({
     name: "FriendshipStatus",
     members: ["PENDING", "ACCEPTED", "REJECTED"],
 });
+
+export const NotificationType = enumType({
+    name: "NotificationType",
+    members: ["LIKE", "COMMENT", "FRIEND_REQUEST"]
+})
+
+export const NotificationEntityType = enumType({
+    name: "NotificationEntityType",
+    members: ["POST", "COMMENT", "USER"]
+})

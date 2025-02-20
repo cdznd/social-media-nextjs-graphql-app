aefd0faea4225a673f2ffba06c553915610361b5
Fabricio Gabriel <floyd9732@gmail.com>
2025-02-20 20:02:29 -0300

    implementing pagination with component

--- src/app/(app)/page.tsx
+++ src/app/(app)/page.tsx
@@ -1,20 +1,28 @@
 import createApolloClient from "@/lib/apollo-client/apolloClient";
 import { GET_PRIVATE_FEED_POSTS } from "@/lib/graphql/fragments/queries/feed";
 import { auth } from "@/lib/next-auth/auth";
-import { Container } from "@mui/system";
+import { Container, Pagination } from "@mui/material";
 import Feed from "@/components/Feed";
 
-import { SearchParamsProps } from "@/types/feed";
+import { HomeSearchParamsProps } from "@/types/feed";
 
-async function getPrivateFeedData(userId: string, searchString?: string, category?: string) {
+async function getPrivateFeedData(
+  userId: string,
+  page: number,
+  searchString?: string,
+  category?: string,
+) {
   const apolloClient = createApolloClient();
   try {
+    const postsPerPage = 10
     const { data } = await apolloClient.query({
       query: GET_PRIVATE_FEED_POSTS,
       variables: {
         userId,
         searchString,
-        category
+        category,
+        take: postsPerPage,
+        skip: (page - 1) * postsPerPage
       },
     });
     return { data, feedError: null };
@@ -25,27 +33,19 @@ async function getPrivateFeedData(userId: string, searchString?: string, categor
 }
 
 export default async function Home(
-  { searchParams }: SearchParamsProps
+  { searchParams }: HomeSearchParamsProps
 ) {
-  const { search, category } = await searchParams
-
-  console.log('Inside paage?');
-  console.log('search', search);
-  console.log('category', category);
-
+  const { search, category, page = 1 } = await searchParams
   const session = await auth()
   // TODO: better handle the feed Error here
   const { data, feedError } = await getPrivateFeedData(
     session?.user?.id!,
+    page,
     search,
     category
   );
-
-  console.log('checking data');
-  console.log(data);
-
   // TODO: Add a type for the feedPosts here, like feedPosts: type
-  const feedPosts = data?.feedPosts ?? []
+  const feedPosts = data?.privateFeedPosts ?? []
   return (
     <Container>
       <Feed

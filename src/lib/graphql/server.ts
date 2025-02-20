014348d7cefaef035f6a260e9674f53e486273a2
Fabricio Gabriel <floyd9732@gmail.com>
2025-02-19 22:03:51 -0300

    Applying graphql query depth limit

--- src/lib/graphql/server.ts
+++ src/lib/graphql/server.ts
@@ -1,10 +1,12 @@
 import { ApolloServer } from "@apollo/server";
+import depthLimit from 'graphql-depth-limit';
 import { Context } from "../prisma/context";
 import { schema } from './schema'
 
 const server = new ApolloServer<Context>({
   schema,
   introspection: process.env.NODE_ENV !== 'production',
+  validationRules: [depthLimit(5)]
 });
 
 export default server

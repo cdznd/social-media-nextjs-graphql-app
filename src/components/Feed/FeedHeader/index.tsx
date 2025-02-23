c2ca44e76f77a5950d7ad0f0cd4daafe3ad906aa
Fabricio Gabriel <floyd9732@gmail.com>
2025-02-23 01:04:02 -0300

    Adding new filter option to feed header

--- src/components/Feed/FeedHeader/index.tsx
+++ src/components/Feed/FeedHeader/index.tsx
@@ -1,8 +1,11 @@
-import { Box } from "@mui/material";
+import { Box, Typography, Button, Chip } from "@mui/material";
 import createApolloClient from "@/lib/apollo-client/apolloClient";
 import { GET_CATEGORIES } from "@/lib/graphql/fragments/queries/category";
 import Search from "./Search";
 import CategorySelector from "./CategorySelector";
+
+import PublicIcon from '@mui/icons-material/Public';
+import LockIcon from '@mui/icons-material/Lock';
 
 async function getCategoriesData() {
     const apolloClient = createApolloClient();
@@ -20,38 +23,84 @@ async function getCategoriesData() {
 export default async function FeedHeader() {
     const { data } = await getCategoriesData();
     return (
-        <Box
-            sx={{
-                display: "flex",
-                flexDirection: { xs: "column-reverse", md: "row" },
-                width: "100%",
-                justifyContent: "space-between",
-                alignItems: { xs: "start", md: "center" },
-                gap: 4,
-                overflow: "auto",
-            }}
-        >
+        <Box>
             <Box
                 sx={{
-                    display: "inline-flex",
-                    flexDirection: "row",
-                    gap: 3,
+                    display: "flex",
+                    flexDirection: { xs: "column-reverse", md: "row" },
+                    width: "100%",
+                    justifyContent: "space-between",
+                    alignItems: { xs: "start", md: "center" },
+                    gap: 4,
                     overflow: "auto",
                 }}
             >
-                <CategorySelector
-                    categories={data.categories} />
+                <Box
+                    sx={{
+                        display: "inline-flex",
+                        flexDirection: "row",
+                        gap: 3,
+                        overflow: "auto",
+                    }}
+                >
+                    <CategorySelector
+                        categories={data.categories} />
+                </Box>
+                <Box
+                    sx={{
+                        display: { xs: "none", sm: "flex" },
+                        flexDirection: "row",
+                        gap: 1,
+                        width: { xs: "100%", md: "fit-content" },
+                        overflow: "auto",
+                    }}
+                >
+                    <Search />
+                </Box>
             </Box>
             <Box
                 sx={{
-                    display: { xs: "none", sm: "flex" },
-                    flexDirection: "row",
-                    gap: 1,
-                    width: { xs: "100%", md: "fit-content" },
+                    // display: "flex",
+                    flexDirection: { xs: "column-reverse", md: "row" },
+                    width: "100%",
+                    justifyContent: "space-between",
+                    alignItems: { xs: "start", md: "center" },
+                    gap: 4,
                     overflow: "auto",
+                    marginTop: '1rem',
+                    display: 'none'
                 }}
             >
-                <Search />
+                <Typography variant="body2">Number of posts: 56</Typography>
+                <Box>
+                    <Button>
+                        <Chip
+                            label={'All'}
+                        />
+                    </Button>
+                    <Button>
+                        <Chip
+                            icon={<PublicIcon />}
+                            color={'success'}
+                            variant='outlined'
+                            label={'Public'}
+                            sx={{
+                                p: 1
+                            }}
+                        />
+                    </Button>
+                    <Button>
+                        <Chip
+                            icon={<LockIcon />}
+                            color={'info'}
+                            variant='outlined'
+                            label={"Private"}
+                            sx={{
+                                p: 1
+                            }}
+                        />
+                    </Button>
+                </Box>
             </Box>
         </Box>
     );

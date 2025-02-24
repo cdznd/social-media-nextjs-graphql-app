8dae9e9a1870a1de92e62f126ce1a1d3ac2b87b8
Fabricio Gabriel <floyd9732@gmail.com>
2025-02-24 17:36:42 -0300

    Cleanup on category selector

--- src/components/Feed/FeedHeader/CategorySelector/index.tsx
+++ src/components/Feed/FeedHeader/CategorySelector/index.tsx
@@ -1,19 +1,21 @@
 'use client'
-
 import { Box, Chip } from '@mui/material';
 import { useRouter, useSearchParams } from 'next/navigation';
-
 import { CategoryType } from '@/types/category';
 import { brand } from '@/components/common/themePrimitives';
 
-export default function CategorySelector({ categories = [] }: { categories: any }) {
+type CategorySelectorProps = {
+    categories: CategoryType[]
+}
+
+export default function CategorySelector(
+    { categories = [] }: CategorySelectorProps) {
 
     const router = useRouter();
     const searchParams = useSearchParams();
     const selectedCategory = searchParams.get("category");
 
     const handleCategoryClick = (category: string) => {
-        // TODO: Better understand this part here
         const params = new URLSearchParams(searchParams.toString());
         if (selectedCategory === category) {
             params.delete("page");
@@ -34,19 +36,22 @@ export default function CategorySelector({ categories = [] }: { categories: any 
                 overflow: "auto",
             }}
         >
-            {categories.map((category: CategoryType) => (
-                <Chip
-                    key={category.id}
-                    onClick={() => handleCategoryClick(category.name)}
-                    size="medium"
-                    variant="filled"
-                    label={category.name}
-                    sx={{
-                        border: selectedCategory === category.name ? "2px solid" : "none",
-                        borderColor: `${brand[500]} !important`
-                    }}
-                />
-            ))}
+            {categories.map((category: CategoryType) => {
+                const isChecked = selectedCategory === category.name
+                return (
+                    <Chip
+                        key={category.id}
+                        onClick={() => handleCategoryClick(category.name)}
+                        size="medium"
+                        variant="filled"
+                        label={category.name}
+                        sx={{
+                            border: isChecked ? "2px solid" : "none",
+                            borderColor: `${brand[500]} !important`,
+                        }}
+                    /> 
+                )
+            })}
         </Box>
     )
 }

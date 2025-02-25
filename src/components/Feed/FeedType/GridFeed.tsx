1da7595ce778243e8298a9c2ad54b7fa0d979521
Fabricio Gabriel <floyd9732@gmail.com>
2025-02-25 18:41:50 -0300

    Style update on grid feed

--- src/components/Feed/FeedType/GridFeed.tsx
+++ src/components/Feed/FeedType/GridFeed.tsx
@@ -10,13 +10,14 @@ export default function GridFeed({ posts }: FeedTypeProps) {
             <Grid item xs={12} sm={6} md={4} lg={3} key={post.id}>
                 <PostCard
                     key={post.id}
-                    postData={post} />
+                    postData={post}
+                    variants={['min-info']} />
             </Grid>
         )
     })
     return (
         <>
-            <Grid container spacing={3} sx={{ py: 4 }}>
+            <Grid container spacing={3} sx={{ py: 4, pt: 0 }}>
                 {renderPosts}
             </Grid>
         </>

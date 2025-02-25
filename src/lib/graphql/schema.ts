3fac2896ebf6ca7c59540fd15842c7a9f48731bc
Fabricio Gabriel <floyd9732@gmail.com>
2025-02-24 21:54:50 -0300

    Code adjustments on schema

--- src/lib/graphql/schema.ts
+++ src/lib/graphql/schema.ts
@@ -236,17 +236,17 @@ const Query = objectType({
                 return notificationService.getNotifications(userId)
             }
         }),
-        t.nonNull.list.nonNull.field('readNotifications', {
-            type: Notification,
-            args: {
-                userId: nonNull(stringArg())
-            },
-            resolve: async (_parent, args, context: Context) => {
-                const { userId } = args
-                const notificationService = new NotificationService(context)
-                return notificationService.getReadNotifications(userId)
-            }
-        })
+            t.nonNull.list.nonNull.field('readNotifications', {
+                type: Notification,
+                args: {
+                    userId: nonNull(stringArg())
+                },
+                resolve: async (_parent, args, context: Context) => {
+                    const { userId } = args
+                    const notificationService = new NotificationService(context)
+                    return notificationService.getReadNotifications(userId)
+                }
+            })
     }
 })
 
@@ -361,18 +361,37 @@ const Mutation = mutationType({
                 return postService.createPost({ title, content, authorId, thumbnail, categories })
             }
         })
-        t.field('triggerLike', {
-            type: Like,
-            args: {
-                userId: nonNull(stringArg()),
-                postId: nonNull(stringArg())
-            },
-            resolve: async (_parent, args, context: Context) => {
-                const { userId, postId } = args
-                const likeService = new LikeService(context)
-                return likeService.triggerLike({ userId, postId })
+        t.field(
+            'triggerLike',
+            {
+                type: Like,
+                args: {
+                    userId: nonNull(stringArg()),
+                    postId: nonNull(stringArg())
+                },
+                resolve: async (_parent, args, context: Context) => {
+                    const { userId, postId } = args
+                    const likeService = new LikeService(context)
+                    const triggerLikeResult = await likeService.triggerLike({ userId, postId })
+                    // Getting user
+                    const userService = new UserService(context)
+                    const user = await userService.getUserById(userId)
+                    // Getting post
+                    const postService = new PostService(context)
+                    const likedPost = await postService.getPostById(postId)
+                    const notificationService = new NotificationService(context)
+                    await notificationService.createNotification({
+                        type: 'LIKE',
+                        content: `Post ${likedPost.title} was liked by ${user?.name}`,
+                        userId: likedPost.authorId, // Sending the notification to the post author id
+                        actorId: userId, // The user that accepted that liked the post
+                        entityType: 'POST',
+                        entityId: postId
+                    })
+                    return triggerLikeResult
+                }
             }
-        })
+        )
         t.field('createCategory', {
             type: Category,
             args: {

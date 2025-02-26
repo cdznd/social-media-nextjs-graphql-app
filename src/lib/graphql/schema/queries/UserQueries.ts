3fe7aebb26520964669e04e19dcace654d422c0e
Fabricio Gabriel <floyd9732@gmail.com>
2025-02-26 12:54:13 -0300

    Re implementing all users resolver

--- src/lib/graphql/schema/queries/UserQueries.ts
+++ src/lib/graphql/schema/queries/UserQueries.ts
@@ -2,12 +2,13 @@ import {
     extendType,
     nonNull,
     stringArg,
-    list
+    intArg
 } from "nexus";
 
 import { Context } from "@/lib/prisma/context";
 import { User } from "../types/Auth"
 import UserService from "@/services/UserService"
+import { DefaultUserListResponse } from "../types/User";
 
 export const UserQueries = extendType({
     type: 'Query',
@@ -23,12 +24,24 @@ export const UserQueries = extendType({
                 return userService.getUserById(userId)
             }
         })
-        t.nonNull.list.nonNull.field('users', {
-            type: User,
-            resolve: async (_parent, args, context: Context) => {
-                const userService = new UserService(context)
-                return userService.getUsers()
+        t.nonNull.field(
+            'allUsers',
+            {
+                type: DefaultUserListResponse,
+                args: {
+                    skip: intArg(),
+                    take: intArg()
+                },
+                resolve: async (_parent, args, context: Context) => {
+                    const { take, skip } = args
+                    const userService = new UserService(context)
+                    const { users, totalCount, totalPages } = await userService.getUsers({
+                        take: take ?? undefined,
+                        skip: skip ?? undefined
+                    })
+                    return { users, totalCount, totalPages }
+                }
             }
-        })      
+        )
     }
 })

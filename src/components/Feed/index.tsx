6241705a638ebc60cb130da335ce7d0ca2889b34
Fabricio Gabriel <floyd9732@gmail.com>
2025-03-02 13:03:40 -0300

    Dynamically importing feed types on feed component

--- src/components/Feed/index.tsx
+++ src/components/Feed/index.tsx
@@ -2,10 +2,12 @@ import { Alert, Box } from '@mui/material';
 import FeedHeader from '@/components/Feed/FeedHeader';
 import PaginationComponent from '@/components/PaginationComponent';
 import BedtimeIcon from '@mui/icons-material/Bedtime';
-import DefaultFeed from './FeedType/DefaultFeed';
-import GridFeed from './FeedType/GridFeed';
-import ExploreFeed from './FeedType/ExploreFeed';
 import { FeedProps } from '@/types/feed';
+import dynamic from 'next/dynamic';
+
+const DefaultFeed = dynamic(() => import('./FeedType/DefaultFeed'))
+const GridFeed = dynamic(() => import('./FeedType/GridFeed'))
+const ExploreFeed = dynamic(() => import('./FeedType/ExploreFeed'))
 
 export default function Feed(
   {

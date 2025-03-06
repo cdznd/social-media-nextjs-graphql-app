b9b79bbbc4e67d71f8f403b82b292e5c9be2bd04
Fabricio Gabriel <floyd9732@gmail.com>
2025-03-05 22:46:39 -0300

    Removing commented code

--- src/lib/apollo-client/apolloClient.ts
+++ src/lib/apollo-client/apolloClient.ts
@@ -1,5 +1,5 @@
 import { ApolloClient, createHttpLink, InMemoryCache } from '@apollo/client';
-// uri: 'http://localhost:3000/api/graphql',
+
 const createApolloClient = () => {
   return new ApolloClient({
     ssrMode: true,

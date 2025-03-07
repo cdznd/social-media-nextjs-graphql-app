f7e764ad9fcce5a24949344325f1b54749e90bf0
Fabricio Gabriel <floyd9732@gmail.com>
2025-03-07 19:08:03 -0300

    chore(general-improvements): imports reordering

--- src/components/Feed/index.tsx
+++ src/components/Feed/index.tsx
@@ -1,11 +1,11 @@
+import dynamic from 'next/dynamic';
 import { Alert, Box } from '@mui/material';
+import BedtimeIcon from '@mui/icons-material/Bedtime';
 import FeedHeader from '@/components/Feed/FeedHeader';
 import PaginationComponent from '@/components/PaginationComponent';
-import BedtimeIcon from '@mui/icons-material/Bedtime';
 import { FeedProps } from '@/types/feed';
-import dynamic from 'next/dynamic';
 
-const DefaultFeed = dynamic(() => import('./FeedType/DefaultFeed'))
+const DefaultFeed = dynamic(() => import('./FeedType/DefaultFeed')) 
 const GridFeed = dynamic(() => import('./FeedType/GridFeed'))
 const ExploreFeed = dynamic(() => import('./FeedType/ExploreFeed'))
 

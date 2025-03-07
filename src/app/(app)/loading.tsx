c2db4fbd4df5dfb7f4f37d5cf9dd18ae53e34b4e
Fabricio Gabriel <floyd9732@gmail.com>
2025-03-07 19:12:00 -0300

    chore(general-improvements): implemeting the loading file for suspense-driven loading

---
+++ src/app/(app)/loading.tsx
@@ -0,0 +1,20 @@
+import { Box, CircularProgress } from "@mui/material"
+
+/**
+ * The loading file will be nested inside the layout
+ * and will automatically wrap the page and any children below it
+ * in a Suspense Boundary.
+ * It acts as a Suspense (React component) displaying Loading UI
+ * while waiting for the server side finish rendering
+ */
+export default function Loading() {
+    return <Box sx={{
+        width: 1,
+        height: 1,
+        display: 'flex',
+        alignItems: 'center',
+        justifyContent: 'center'
+    }}>
+        <CircularProgress />
+    </Box>
+}

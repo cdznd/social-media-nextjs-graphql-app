a52a1e1f43a6089fb8c5ed5a00d9ee1080e53997
Fabricio Gabriel <floyd9732@gmail.com>
2025-03-07 21:08:05 -0300

    chore(general-improvements): improvements on user profile card style adding next link

--- src/components/UserProfileCard/index.tsx
+++ src/components/UserProfileCard/index.tsx
@@ -1,8 +1,9 @@
+import Image from "next/image";
+import Link from "next/link";
 import { Box, Card, CardContent, Avatar, Typography } from "@mui/material"
 import { brand } from "../common/themePrimitives";
 import FriendshipTriggerButton from "../FriendshipTriggerButton";
 import { UserProfileCardProps } from "@/types/user";
-import Image from "next/image";
 
 export default function UserProfileCard(
   { user }: UserProfileCardProps
@@ -36,26 +37,24 @@ export default function UserProfileCard(
       </Avatar>
       <CardContent sx={{ display: "flex", flexDirection: "column", alignItems: "start" }}>
         <Box sx={{
-          marginBottom: '2rem',
+          marginBottom: '1.5rem',
           width: 1,
           textAlign: 'center'
         }}>
-          <Box
-            component={'a'}
-            href={`users/${user.id}`}
-            sx={{
-              textDecoration: 'none',
-              color: 'inherit',
-              transition: '100ms',
-              '&:hover': {
-                color: brand[400]
-              }
-            }}
-          >
-            <Typography variant="h6" sx={{ textDecoration: 'none' }}>
-              {user.name}
-            </Typography>
-          </Box>
+          <Link
+            href={`/users/${user.id}`}
+            style={{ textDecoration: 'none', color: 'inherit' }}>
+            <Typography 
+              variant="h6"
+              sx={{
+                textDecoration: 'none',
+                color: 'inherit',
+                transition: '100ms',
+                '&:hover': {
+                  color: brand[400]
+                }
+              }}>{user.name}</Typography>
+          </Link>
         </Box>
         <FriendshipTriggerButton
           toUserId={user.id}

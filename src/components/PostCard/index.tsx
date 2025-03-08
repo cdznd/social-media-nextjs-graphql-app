0244bc7ebbbb3c047d3131be52ef468ee3e3f20c
Fabricio Gabriel <floyd9732@gmail.com>
2025-03-08 18:46:51 -0300

    fix(general-improvements): main updates on post card

--- src/components/PostCard/index.tsx
+++ src/components/PostCard/index.tsx
@@ -1,37 +1,20 @@
-'use client'
-
-import {
-    Chip,
-    Box,
-    CardMedia,
-    Avatar,
-    Typography,
-    Stack
-} from '@mui/material';
-
 import Image from 'next/image';
 import Link from 'next/link';
-
-import { gray } from '../common/themePrimitives';
-
 import {
-    StyledPostCard,
-    StyledPostCardInfo,
-    StyledPostCardContent,
-    StyledTypography,
-    StyledPostCardCategories
-} from './style'
-
+    Box,
+    Typography,
+    Stack,
+    Card,
+    CardMedia,
+    CardContent
+} from '@mui/material';
 import { brand } from '../common/themePrimitives';
-
-import PublicIcon from '@mui/icons-material/Public';
-import LockIcon from '@mui/icons-material/Lock';
-
 import months from '@/utils/months';
-
+import PostEngagement from '../PostEngagement';
+import PostAuthorChip from '../PostAuthorChip';
+import PostVisibilityStatus from '../PostVisibilityStatus';
+import PostCategoryChip from '../PostCategoryChip';
 import { PostType } from '@/types/post';
-
-import PostEngagement from './PostEngagement';
 
 type PostCardProps = {
     postData: PostType,
@@ -41,153 +24,157 @@ type PostCardProps = {
 
 export default function PostCard({ postData, variants = [], position }: PostCardProps) {
 
-    const author = postData?.author
     const createdAt = postData?.createdAt
     const creationDate = new Date(createdAt)
     const displayDate = `${creationDate.getDate()} ${months[creationDate.getMonth()]} ${creationDate.getFullYear()}`
-
+    const postAuthor = postData?.author
     const postVisibility = postData?.visibility
-
-    // TODO: Add a click to send to the category filter feed
-    const renderCategories =
-        postData?.categories && postData?.categories.length > 0 ?
-            postData?.categories.map(category => {
-                return (
-                    <Chip
-                        key={category?.name}
-                        variant="filled"
-                        label={category?.name}
-                        size='medium'
-                    />
-                )
-            }) : null
+    const postCategories = postData?.categories ?? []
+    const postLikes = postData?.likes ?? []
+    const postComments = postData?.comments ?? []
 
     // min-info variation does not include image, engagement, or categories
     const minInfo = variants.includes('min-info')
     const disabledEngagement = variants.includes('disable-user-engagement')
 
-    const postLikes = postData?.likes ?? []
-    const postComments = postData?.comments ?? []
-
-    /**
-     * When using the property fill from next/image you should have a relative component with a defined height.
-     */
-    const postImageHeight = 400
+    const postMediaHeight = 400
 
     return (
-        <StyledPostCard
-            variant="outlined"
+        <Card
             tabIndex={0}
+            sx={{
+                display: 'flex',
+                flexDirection: 'column',
+                gap: '8px',
+                padding: 0,
+                height: '100%',
+                backgroundColor: 'background.paper',
+                '&:hover': {
+                    cursor: 'pointer',
+                },
+                '&:focus-visible': {
+                    outline: '3px solid',
+                    outlineColor: 'hsla(210, 98%, 48%, 0.5)',
+                    outlineOffset: '2px',
+                },
+                minHeight: '400px'
+            }}
         >
+            {/* Post Media */}
             {
-                (postData?.thumbnail) && (
-                    <CardMedia sx={{ height: postImageHeight, position: 'relative' }}>
+                postData?.thumbnail && (
+                    <CardMedia sx={{ height: postMediaHeight, position: 'relative' }}>
                         <Image
                             src={postData?.thumbnail}
-                            fill
+                            fill // When using the property fill from next/image you should have a relative component with a defined height.
                             alt={postData?.title}
                             style={{ objectFit: 'cover' }} // Ensures proper scaling
-                            priority={position ? position <= 2 : false}
+                            priority={position ? position <= 2 : false} // Disabling lazy loading for the first images
                         />
                     </CardMedia>
                 )
             }
-
-            <StyledPostCardInfo>
+            {/* Post Author + Date Info */}
+            <Stack
+                direction='row'
+                alignItems='center'
+                justifyContent='space-between'
+                sx={{
+                    backgroundColor: 'background.default',
+                    padding: 2,
+                    paddingTop: 1,
+                    paddingBottom: 1,
+                    borderBottom: '1px solid',
+                    borderTop: '1px solid',
+                    borderColor: brand[200]
+                }}
+            >
                 {
                     !minInfo && (
-                        <Link href={`/users/${author.id}`} style={{ textDecoration: 'none', color: 'inherit' }}>
-                            <Box
-                                sx={{
-                                    display: 'flex',
-                                    flexDirection: 'row',
-                                    gap: 1,
-                                    alignItems: 'center',
-                                    paddingY: '.3rem',
-                                    paddingX: '.5rem',
-                                    border: '1px solid',
-                                    borderColor: gray[700],
-                                    borderRadius: '1rem',
-                                    transition: '200ms',
-                                    '&:hover': {
-                                        background: brand[800]
-                                    },
-                                }}
-                            >
-                                <Avatar
-                                    sx={{ 
-                                        width: 24,
-                                        height: 24,
-                                        border: '1px solid',
-                                        borderColor: gray[400]
-                                    }}
-                                >
-                                    <Image
-                                        src={author.image}
-                                        width={24}
-                                        height={24}
-                                        alt={postData?.title}
-                                        quality={75}
-                                        style={{ objectFit: 'cover' }}
-                                    />
-                                </Avatar>
-                                <Typography variant="caption" sx={{ fontWeight: 'bold' }}>
-                                    {author.name}
-                                </Typography>
-                            </Box>
-                        </Link>
+                        <PostAuthorChip author={postAuthor} />
                     )
                 }
                 <Typography variant="caption" sx={{ fontWeight: 'bold' }}>
                     {displayDate}
                 </Typography>
-            </StyledPostCardInfo>
-
-            <StyledPostCardContent>
-                <Stack direction="row" sx={{ mb: 2 }}>
-                    <Chip
-                        icon={postVisibility === 'PUBLIC' ? <PublicIcon /> : <LockIcon />}
-                        color={postVisibility === 'PUBLIC' ? 'success' : 'info'}
-                        variant='outlined'
-                        label={postVisibility === 'PUBLIC' ? 'Public' : "Private"}
-                        sx={{
-                            p: 1
-                        }}
-                    />
+            </Stack>
+            <CardContent
+                sx={{
+                    padding: 2,
+                    paddingTop: 1,
+                }}
+            >
+                {/* Visibility */}
+                <Stack direction="row" sx={{ mb: 1 }}>
+                    <PostVisibilityStatus
+                        postVisibility={postVisibility} />
                 </Stack>
-                {
-                    !minInfo ? (
-                        <Link href={`/post/${postData.id}`} style={{ textDecoration: 'none', color: 'inherit' }}>
-                            <Typography variant='h6' sx={{ '&:hover': { color: 'red' }}}>{postData.title}</Typography>
-                        </Link>
-                    ) : (
-                        <Link href={`/post/${postData.id}`} style={{ textDecoration: 'none', color: 'inherit' }}>
-                            <Typography variant='body2' sx={{ fontWeight: 'bold', textAlign: 'start', mb: 1 }}>{postData.title}</Typography>
-                        </Link>
-                    )
-                }
-                <StyledTypography color="text.secondary" gutterBottom>
+                {/* Title LInk */}
+                <Link
+                    href={`/post/${postData.id}`}
+                    style={{ textDecoration: 'none', color: 'inherit' }}>
+                    <Typography
+                        variant={minInfo ? 'body2' : 'h5'}
+                        color="text.primary"
+                        sx={{
+                            '&:hover': {
+                                color: brand[400]
+                            },
+                            ...(minInfo
+                                ? { fontWeight: 'bold', textAlign: 'start', mb: 1 }
+                                : {}
+                            )
+                        }}
+                    >
+                        {postData.title}
+                    </Typography>
+                </Link>
+                {/* Post Excerpt */}
+                <Typography
+                    color="text.secondary"
+                    sx={{
+                        display: '-webkit-box',
+                        WebkitBoxOrient: 'vertical',
+                        WebkitLineClamp: 3,
+                        overflow: 'hidden',
+                        textOverflow: 'ellipsis',
+                        textAlign: 'start'
+                    }}
+                    gutterBottom
+                >
                     {postData.content}
-                </StyledTypography>
-            </StyledPostCardContent>
-
+                </Typography>
+            </CardContent>
             <Box sx={{ flexGrow: 1 }}></Box>
-
             {
                 !minInfo && (
-                    <StyledPostCardCategories direction="row" spacing={2}>
-                        {renderCategories}
-                    </StyledPostCardCategories>
+                    <Stack
+                        direction="row"
+                        spacing={1}
+                        sx={{
+                            padding: 2
+                        }}
+                    >
+                        {
+                            postCategories.length > 0 && (
+                                postCategories.map(category => {
+                                    return (
+                                        <PostCategoryChip
+                                            category={category} />
+                                    )
+                                })
+                            )
+                        }
+                    </Stack>
                 )
             }
-
             <PostEngagement
                 postId={postData?.id}
                 likes={postLikes}
                 comments={postComments}
                 isDisabled={disabledEngagement}
             />
-        </StyledPostCard>
+        </Card>
     );
 
 }

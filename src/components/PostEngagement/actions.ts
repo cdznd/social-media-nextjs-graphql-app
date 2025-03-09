25a6234c3231de3c4254ece06a3c60dc5bc587c8
Fabricio Gabriel <floyd9732@gmail.com>
2025-03-09 18:53:04 -0300

    feat(general-improvements): implementing server action with status on response

--- src/components/PostEngagement/actions.ts
+++ src/components/PostEngagement/actions.ts
@@ -1,16 +1,28 @@
-'use server'
+'use server' // Marking use server to make all exports server actions.
 import createApolloClient from "@/lib/apollo-client/apolloClient";
 import { TRIGGER_POST_LIKE_MUTATION } from "@/fragments/mutations/mutations";
-
+ 
 const apolloClient = createApolloClient();
 
 export async function triggerLike(userId: string, postId: string) {
-    const likedPostResult = await apolloClient.mutate({
-        mutation: TRIGGER_POST_LIKE_MUTATION,
-        variables: {
-            userId,
-            postId
+    try {
+        const likedPostResult = await apolloClient.mutate({
+            mutation: TRIGGER_POST_LIKE_MUTATION,
+            variables: {
+                userId,
+                postId
+            }
+        })
+        return {
+            success: true,
+            likes: likedPostResult?.data?.triggerLike?.likes ?? []
         }
-    })
-    return likedPostResult?.data?.triggerLike?.likes ?? []
+    } catch(error) {
+        console.error(error)
+        return { // Modeling expected errors as return values
+            success: false,
+            error: "Failed to like the post. Please try again later.",
+            likes: []
+        }
+    }
 }

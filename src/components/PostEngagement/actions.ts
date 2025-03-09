c8b7ad081f618186b316ad75935cd078dccd7eea
Fabricio Gabriel <floyd9732@gmail.com>
2025-03-09 19:26:49 -0300

    feat(general-improvements): ensuring users are authorized when executing server action

--- src/components/PostEngagement/actions.ts
+++ src/components/PostEngagement/actions.ts
@@ -1,11 +1,19 @@
 'use server' // Marking use server to make all exports server actions.
 import createApolloClient from "@/lib/apollo-client/apolloClient";
 import { TRIGGER_POST_LIKE_MUTATION } from "@/fragments/mutations/mutations";
- 
+
+import { auth } from "@/lib/next-auth/auth";
+
 const apolloClient = createApolloClient();
 
 export async function triggerLike(userId: string, postId: string) {
     try {
+        // Ensuring the user is authorized to perform the action
+        const session = await auth()
+        const loggedUserId = session?.user?.id
+        if(loggedUserId !== userId) {
+            throw new Error('Error with logged user')
+        }
         const likedPostResult = await apolloClient.mutate({
             mutation: TRIGGER_POST_LIKE_MUTATION,
             variables: {
@@ -19,7 +27,8 @@ export async function triggerLike(userId: string, postId: string) {
         }
     } catch(error) {
         console.error(error)
-        return { // Modeling expected errors as return values
+        // Modeling expected errors as return values
+        return {
             success: false,
             error: "Failed to like the post. Please try again later.",
             likes: []

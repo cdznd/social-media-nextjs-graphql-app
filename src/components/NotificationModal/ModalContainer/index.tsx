757adfe9415c34878f5d28ad68a1054e9e6bf374
Fabricio Gabriel <floyd9732@gmail.com>
2025-03-09 20:57:19 -0300

    feat(general-improvements): creating notification modal header and shared container

---
+++ src/components/NotificationModal/ModalContainer/index.tsx
@@ -0,0 +1,36 @@
+import { Modal, Box } from '@mui/material'
+
+type ModalContainerProps = {
+    open: boolean,
+    onClose: () => void,
+    children: React.ReactNode
+}
+
+export default function ModalContainer(
+    { open, onClose, children }: ModalContainerProps
+) {
+    return (
+        <Modal
+            open={open}
+            onClose={onClose}
+            aria-labelledby="notifications-modal"
+        >
+            <Box sx={{
+                position: 'absolute',
+                top: '50%',
+                left: '50%',
+                transform: 'translate(-50%, -50%)',
+                width: 800,
+                maxHeight: '80%',
+                overflowY: 'scroll',
+                bgcolor: 'background.paper',
+                boxShadow: 24,
+                borderRadius: '1rem',
+                paddingY: '1.5rem',
+                paddingX: '2rem'
+            }}>
+                {children}
+            </Box>
+        </Modal>
+    )
+}

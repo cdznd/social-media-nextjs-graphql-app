165011d99d4d3dca7b7daf385ade8cf76ed65850
Fabricio Gabriel <floyd9732@gmail.com>
2025-03-12 22:55:59 -0300

    feat(post-comments): adding page components to post id page

--- src/app/(app)/post/[postId]/page.tsx
+++ src/app/(app)/post/[postId]/page.tsx
@@ -1,11 +1,21 @@
+import Image from "next/image"
+import Link from "next/link"
+
 import createApolloClient from "@/lib/apollo-client/apolloClient"
 import { GET_POST } from "@/fragments/queries/post"
+
 import { Typography, Container, Box, Stack, IconButton, Chip } from "@mui/material"
-import Image from "next/image"
-import PostAuthorChip from "@/components/PostAuthorChip"
-import ErrorAlert from "@/components/ErrorAlert"
 import ArrowBackIcon from '@mui/icons-material/ArrowBack';
+
 import { gray } from "@/components/common/themePrimitives"
+
+import PostVisibilityStatus from "@/components/PostVisibilityStatus"
+import PostCategoryChip from "@/components/PostCategoryChip"
+import PostEngagement from "@/components/PostEngagement"
+import PostAuthorChip from "@/components/PostAuthorChip"
+import PostComments from "@/components/PostComments"
+import ErrorAlert from "@/components/ErrorAlert"
+
 import { CategoryType } from "@/types/category"
 
 type PostPageParams = Promise<{
@@ -33,28 +43,23 @@ export default async function PostPage(
 ) {
     const { postId } = await props.params
     const { data } = await getPostData(postId)
-
     const postData = data?.post
+
+    // console.log('postData');
+    // console.log(postData);
+
     const postAuthor = postData?.author
+    const postCategories = postData?.categories
+    const postLikes = postData?.likes ?? []
+    const postComments = postData?.comments ?? []
 
     if (!postData) return <ErrorAlert message="No Post Found" />
 
-    const renderCategories =
-        postData?.categories && postData?.categories.length > 0 ?
-            postData?.categories.map((category: CategoryType) => {
-                return (
-                    <Chip
-                        key={category?.name}
-                        variant="filled"
-                        label={category?.name}
-                        size='medium'
-                    />
-                )
-            }) : null
+    const postImageHeight = 500
 
     return (
         <Container>
-
+            {/* Back button */}
             <Stack
                 direction="row"
                 justifyContent="start"
@@ -63,55 +68,115 @@ export default async function PostPage(
                     mb: 4
                 }}
             >
-                <IconButton aria-label="back">
-                    <ArrowBackIcon />
-                </IconButton>
+                <Link
+                    href='/feed'
+                    style={{ textDecoration: 'none', color: 'inherit' }}
+                >
+                    <IconButton aria-label="back">
+                        <ArrowBackIcon />
+                    </IconButton>
+                </Link>
             </Stack>
-
-            <Box sx={{
-                border: '1px solid',
-                borderColor: gray[700],
-                borderRadius: 1,
-                p: 4
-            }}>
+            {/* Post Authos + Title + visibility  */}
+            <Stack
+                direction="column"
+                justifyContent="start"
+                spacing={1}
+                sx={{
+                    mb: 4
+                }}
+            >
                 <Stack
-                    direction="column"
-                    justifyContent="start"
-                    spacing={1}
+                    direction="row"
+                    justifyContent="space-between"
+                    alignItems="center"
                     sx={{
-                        mb: 4
+                        w: 1
                     }}
                 >
-                    <Stack
-                        direction="row"
-                        justifyContent="start"
-                    >
-                        <PostAuthorChip
-                            author={postAuthor} />
-                    </Stack>
-                    <Typography variant="h3">{postData.title}</Typography>
+                    <PostAuthorChip
+                        author={postAuthor} />
+                    <PostVisibilityStatus
+                        postVisibility={postData?.visibility} />
                 </Stack>
-
+                <Typography
+                    variant="h3"
+                    color="text.primary">
+                    {postData.title}
+                </Typography>
+            </Stack>
+            {/* Post Categories + Content */}
+            <Stack
+                direction="column"
+                spacing={1}
+            >
                 <Stack
-                    direction="column"
+                    direction="row"
                     spacing={1}
                 >
-                    <Box>{renderCategories}</Box>
-                    <Box>{postData.content}</Box>
+                    {
+                        postCategories.length > 0 && (
+                            postCategories.map((category: CategoryType) => {
+                                return (
+                                    <PostCategoryChip
+                                        key={category.id}
+                                        category={category} />
+                                )
+                            })
+                        )
+                    }
                 </Stack>
-
-                <Box sx={{ w: 1, height: '400px', position: 'relative', borderRadius: '1rem', mb: 4, mt: 2 }}>
-                    <Image
-                        src={postData?.thumbnail}
-                        fill={true}
-                        alt={postData.title}
-                        style={{
-                            objectFit: 'cover',
-                            borderRadius: '1rem'
-                        }}
-                    />
-                </Box>
-
+                <Typography
+                    variant="body2"
+                    color="text.secondary">
+                    {postData.content}
+                </Typography>
+            </Stack>
+            {/* Post Image */}
+            <Box sx={{
+                w: 1,
+                height: postImageHeight,
+                position: 'relative',
+                borderRadius: 8,
+                mt: 2
+            }}>
+                <Image
+                    src={postData?.thumbnail}
+                    fill={true}
+                    alt={postData.title}
+                    style={{
+                        objectFit: 'cover',
+                        borderRadius: 8
+                    }}
+                />
+            </Box>
+            {/* Post Engagement */}
+            <Box
+                sx={{
+                    border: '1px solid',
+                    borderColor: 'divider',
+                    borderRadius: 1,
+                    mt: 2
+                }}
+            >
+                <PostEngagement
+                    postId={postId}
+                    likes={postLikes}
+                    comments={postComments}
+                    isDisabled={false}
+                    variants={['isPostPage']}
+                />
+            </Box>
+            {/* Post Comments */}
+            <Box
+                sx={{
+                    mt: 4
+                }}
+            >
+                <Typography variant="h6" color="text.primary">Comments</Typography>
+                <PostComments
+                    postId={postId}
+                    comments={postComments} />
             </Box>
         </Container>
     )

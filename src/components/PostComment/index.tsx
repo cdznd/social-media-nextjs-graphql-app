8a0778e5a5d16388c09691f071b87cf4b8027710
Fabricio Gabriel <floyd9732@gmail.com>
2025-03-13 21:21:33 -0300

    feat(post-comments): adding post options to post comment based on logged user id

--- src/components/PostComment/index.tsx
+++ src/components/PostComment/index.tsx
@@ -1,10 +1,19 @@
 import Link from "next/link"
 import { Box, Divider, Stack, Typography } from "@mui/material"
 import UserAvatar from "../UserAvatar"
-import { CommentType } from "@/types/comment"
 import { brand } from "../common/themePrimitives"
+import { CommentType } from "@/types/comment"
+import PostCommentOptions from "../PostCommentOptions"
 
-export default function PostComment({ comment }: { comment: CommentType }) {
+type PostCommentsProps = {
+    comment: CommentType,
+    loggedUserId: string,
+    postId: string
+}
+
+export default function PostComment(
+    { comment, loggedUserId, postId }: PostCommentsProps
+) {
     const commentAuthor = comment?.user ?? {}
     const commentCreationDate = new Date(comment?.createdAt)
     const formattedDate = commentCreationDate.toLocaleString("pt-BR", {
@@ -28,37 +37,47 @@ export default function PostComment({ comment }: { comment: CommentType }) {
         >
             <Stack
                 direction="row"
-                justifyContent="start"
+                justifyContent="space-between"
                 alignItems="center"
                 spacing={1}
             >
-                <UserAvatar
-                    userImage={commentAuthor?.image}
-                    size={{
-                        height: 30,
-                        width: 30
-                    }}
-                />
-                <Link
-                    href={`/users/${commentAuthor.id}`}
-                    style={{ textDecoration: 'none', color: 'inherit' }}>
+                <Stack
+                    direction="row"
+                    justifyContent="start"
+                    alignItems="center"
+                    spacing={1}
+                >
+                    <UserAvatar
+                        userImage={commentAuthor?.image}
+                        size={{
+                            height: 30,
+                            width: 30
+                        }}
+                    />
+                    <Link
+                        href={`/users/${commentAuthor.id}`}
+                        style={{ textDecoration: 'none', color: 'inherit' }}>
+                        <Typography
+                            variant="body1"
+                            color="text.primary"
+                            sx={{
+                                fontWeight: '600',
+                                '&:hover': {
+                                    color: brand[400]
+                                }
+                            }}>
+                            {commentAuthor.name}
+                        </Typography>
+                    </Link>
                     <Typography
-                        variant="body1"
-                        color="text.primary"
-                        sx={{
-                            fontWeight: '600',
-                            '&:hover': {
-                                color: brand[400]
-                            }
-                        }}>
-                        {commentAuthor.name}
+                        variant="caption"
+                        color="text.secondary">
+                        {formattedDate}
                     </Typography>
-                </Link>
-                <Typography
-                    variant="caption"
-                    color="text.secondary">
-                    {formattedDate}
-                </Typography>
+                </Stack>
+                {
+                    (loggedUserId === commentAuthor.id) && <PostCommentOptions comment={comment} postId={postId} />
+                }
             </Stack>
             <Divider sx={{ my: 1 }} />
             <Box
@@ -73,6 +92,6 @@ export default function PostComment({ comment }: { comment: CommentType }) {
                     {comment.content}
                 </Typography>
             </Box>
-        </Box>
+        </Box >
     )
 }

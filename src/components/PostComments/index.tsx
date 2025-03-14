cb369ea3d7cd0a279ed730f3a95180445fd532c0
Fabricio Gabriel <floyd9732@gmail.com>
2025-03-13 21:23:32 -0300

    feat(post-comments): adding post id and logged user id props to post comment

--- src/components/PostComments/index.tsx
+++ src/components/PostComments/index.tsx
@@ -1,23 +1,29 @@
 import { CommentType } from "@/types/comment"
 import PostCommentForm from "../PostCommentForm"
 import PostComment from "../PostComment"
+import { auth } from "@/lib/next-auth/auth"
 
 type PostCommentsProps = {
     postId: string,
     comments: CommentType[]
 }
 
-export default function PostComments({ postId, comments }: PostCommentsProps) {
+export default async function PostComments({ postId, comments }: PostCommentsProps) {
+    const session = await auth()
+    const loggedUserId = session?.user?.id ?? ''
     return (
         <>
             <PostCommentForm
                 postId={postId}
+                loggedUserId={loggedUserId}
             />
             {
                 comments.map((comment: CommentType) => {
                     return <PostComment
                         key={comment?.id}
-                        comment={comment} />
+                        comment={comment}
+                        loggedUserId={loggedUserId}
+                        postId={postId} />
                 })
             }
         </>

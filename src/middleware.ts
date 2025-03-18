b97472f70741456a525364fe88e3c011fe01ecdc
Fabricio Gabriel <floyd9732@gmail.com>
2025-03-17 22:35:54 -0300

    refactor(middleware): moving public paths to another file

--- src/middleware.ts
+++ src/middleware.ts
@@ -1,16 +1,15 @@
 import { NextResponse } from 'next/server';
 import type { NextRequest } from 'next/server';
 import { getToken } from 'next-auth/jwt'; // Used server side to get JWT Tokens
+import { publicPaths } from './lib/routes';
 
 export async function middleware(request: NextRequest) {
   const token = await getToken({ req: request });
   const isAuthenticated = !!token;
 
   const { pathname } = request.nextUrl;
-
-  const publicPaths = ['/sign-in', '/sign-up', '/home', '/api/auth/signin', '/api/auth/signup'];
   const isPublicPath = publicPaths.some(path => request.nextUrl.pathname.startsWith(path));
-
+  
   if (pathname.startsWith('/api/graphql')) {
     return NextResponse.next();
   }

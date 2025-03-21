e4eeb7e5f13a70f4b1ae18a1cc60ec88c662c1d3
Fabricio Gabriel <floyd9732@gmail.com>
2025-03-20 22:42:30 -0300

    refactor(sign-in-up): improvements on sign in and sign up page, making the page server component and nested components client

--- src/app/sign-in/page.tsx
+++ src/app/sign-in/page.tsx
@@ -1,70 +1,58 @@
-"use client"
-
 import {
-    Box,
-    Button,
-    Divider,
-    Link,
-    Typography
+    Typography,
+    Card,
+    Stack
 } from '@mui/material'
-import CardAuth from '@/components/Authentication/AuthCard';
-import AuthContainer from '@/components/Authentication/AuthContainer';
 import CredentialsSigninForm from '@/components/Authentication/CredentialsSigninForm';
-import { GoogleIcon, SitemarkIcon } from '@/components/common/CustomIcons'
-
+import { SitemarkIcon } from '@/components/common/CustomIcons'
 import ColorModeSelect from '@/components/common/ColorModeSelect';
 
-import { signIn } from 'next-auth/react';
-
 export default function SignInPage() {
-
-    const handleGoogleSignIn = async () => {
-        const result = await signIn('google', {
-            callbackUrl: '/',
-            redirect: true,
-        });
-        if (result?.error) {
-            console.error(result.error);
-        }
-    };
-
     return (
-        <AuthContainer direction="column" justifyContent="space-between">
+        <Stack
+            direction="column"
+            justifyContent="space-between"
+            sx={{
+                height: 'calc((1 - var(--template-frame-height, 0)) * 100dvh)',
+                minHeight: '100%',
+                padding: 2,
+                '&::before': {
+                    content: '""',
+                    display: 'block',
+                    position: 'absolute',
+                    zIndex: -1,
+                    inset: 0,
+                    backgroundImage:
+                        'radial-gradient(ellipse at 50% 50%, hsl(210, 100%, 97%), hsl(0, 0%, 100%))',
+                    backgroundRepeat: 'no-repeat',
+                },
+            }}
+        >
             <ColorModeSelect sx={{ position: 'fixed', top: '1rem', right: '1rem' }} />
-            <CardAuth variant="outlined">
+            <Card
+                variant="outlined"
+                sx={{
+                    display: 'flex',
+                    flexDirection: 'column',
+                    alignSelf: 'center',
+                    overflowY: 'scroll',
+                    width: '100%',
+                    maxWidth: '500px',
+                    padding: 4,
+                    gap: 2,
+                    margin: 'auto',
+                    boxShadow: 'hsla(220, 30%, 5%, 0.05) 0px 5px 15px 0px, hsla(220, 25%, 10%, 0.05) 0px 15px 35px -5px',
+                }}
+            >
                 <SitemarkIcon />
                 <Typography
                     component="h1"
                     variant="h4"
-                    sx={{ width: '100%', fontSize: 'clamp(2rem, 10vw, 2.15rem)' }}
                 >
-                    Sign in
+                    Sign In
                 </Typography>
                 <CredentialsSigninForm />
-                <Divider>
-                    <Typography sx={{ color: 'text.secondary' }}>or</Typography>
-                </Divider>
-                <Box sx={{ display: 'flex', flexDirection: 'column', gap: 2 }}>
-                    <Button
-                        fullWidth
-                        variant="outlined"
-                        onClick={() => handleGoogleSignIn()}
-                        startIcon={<GoogleIcon />}
-                    >
-                        Sign in with Google
-                    </Button>
-                    <Typography sx={{ textAlign: 'center' }}>
-                        Don&apos;t have an account?{' '}
-                        <Link
-                            href="/sign-up/"
-                            variant="body2"
-                            sx={{ alignSelf: 'center' }}
-                        >
-                            Sign up
-                        </Link>
-                    </Typography>
-                </Box>
-            </CardAuth>
-        </AuthContainer>
+            </Card>
+        </Stack>
     )
 }
--- src/app/sign-up/page.tsx
+++ src/app/sign-up/page.tsx
@@ -1,70 +1,59 @@
-"use client"
-
 import {
-    Box,
-    Button,
-    Divider,
-    Link,
-    Typography
+    Typography,
+    Stack,
+    Card
 } from '@mui/material'
-import CardAuth from '@/components/Authentication/AuthCard';
-import AuthContainer from '@/components/Authentication/AuthContainer';
 import CredentialsSignupForm from '@/components/Authentication/CredentialsSignupForm';
-import { GoogleIcon, SitemarkIcon } from '@/components/common/CustomIcons';
-
+import { SitemarkIcon } from '@/components/common/CustomIcons';
 import ColorModeSelect from '@/components/common/ColorModeSelect';
-
-import { signIn } from 'next-auth/react';
 
 export default function SignUpPage() {
 
-    const handleSignUp = async () => {
-        const result = await signIn('google', {
-            callbackUrl: '/',
-            redirect: true,
-        });
-        if (result?.error) {
-            console.error(result.error);
-        }
-    };
-
     return (
-        <AuthContainer direction="column" justifyContent="space-between">
+        <Stack
+            direction="column"
+            justifyContent="space-between"
+            sx={{
+                height: 'calc((1 - var(--template-frame-height, 0)) * 100dvh)',
+                minHeight: '100%',
+                padding: 2,
+                '&::before': {
+                    content: '""',
+                    display: 'block',
+                    position: 'absolute',
+                    zIndex: -1,
+                    inset: 0,
+                    backgroundImage:
+                        'radial-gradient(ellipse at 50% 50%, hsl(210, 100%, 97%), hsl(0, 0%, 100%))',
+                    backgroundRepeat: 'no-repeat',
+                },
+            }}    
+        >
             <ColorModeSelect sx={{ position: 'fixed', top: '1rem', right: '1rem' }} />
-            <CardAuth variant="outlined">
+            <Card
+                variant="outlined"
+                sx={{
+                    display: 'flex',
+                    flexDirection: 'column',
+                    alignSelf: 'center',
+                    overflowY: 'scroll',
+                    width: '100%',
+                    maxWidth: '500px',
+                    padding: 4,
+                    gap: 2,
+                    margin: 'auto',
+                    boxShadow: 'hsla(220, 30%, 5%, 0.05) 0px 5px 15px 0px, hsla(220, 25%, 10%, 0.05) 0px 15px 35px -5px',
+                }}    
+            >
                 <SitemarkIcon />
                 <Typography
                     component="h1"
                     variant="h4"
-                    sx={{ width: '100%', fontSize: 'clamp(2rem, 10vw, 2.15rem)' }}
                 >
-                    Sign up
+                    Sign Up
                 </Typography>
                 <CredentialsSignupForm />
-                <Divider>
-                    <Typography sx={{ color: 'text.secondary' }}>or</Typography>
-                </Divider>
-                <Box sx={{ display: 'flex', flexDirection: 'column', gap: 2 }}>
-                    <Button
-                        fullWidth
-                        variant="outlined"
-                        onClick={() => handleSignUp()}
-                        startIcon={<GoogleIcon />}
-                    >
-                        Sign up with Google
-                    </Button>
-                    <Typography sx={{ textAlign: 'center' }}>
-                        Already have an account?{' '}
-                        <Link
-                            href="/sign-in/"
-                            variant="body2"
-                            sx={{ alignSelf: 'center' }}
-                        >
-                            Sign in
-                        </Link>
-                    </Typography>
-                </Box>
-            </CardAuth>
-        </AuthContainer>
+            </Card>
+        </Stack>
     );
 }

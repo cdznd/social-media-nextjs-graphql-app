6ef2b6a5974bddfb2019394dc0acc584211e568c
Fabricio Gabriel <floyd9732@gmail.com>
2025-03-20 22:41:10 -0300

    refactor(regular-expression): improving email regex function

--- src/utils/email.ts
+++ src/utils/email.ts
@@ -1,3 +1,5 @@
-const validateEmailFormat = (email: string) => {
-    return !/\S+@\S+\.\S+/.test(email)
+export const validateEmailFormat = (email: string) => {
+    // Regular expression literal
+    const emailRegex = /^[a-zA-Z0-9._%+-]+@[a-zA-Z0-9.-]+\.com$/;
+    return emailRegex.test(email);
 }

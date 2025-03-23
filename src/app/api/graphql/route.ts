793183f2005f2a5118b8e5d950e03a078b5e4bfd
Fabricio Gabriel <floyd9732@gmail.com>
2025-03-23 18:51:43 -0300

    refactor(route): removing comments

--- src/app/api/graphql/route.ts
+++ src/app/api/graphql/route.ts
@@ -1,15 +1,10 @@
 import { NextRequest } from "next/server";
 import { startServerAndCreateNextHandler } from "@as-integrations/next";
-// import { getToken } from "next-auth/jwt";
 import server from "@/lib/graphql/server";
 import { createContext, Context } from "@/lib/prisma/context";
 
 const handler = startServerAndCreateNextHandler<NextRequest, Context>(server, {
   context: async () => {
-    // const token = await getToken({ req });
-    // if (process.env.NODE_ENV === 'production' && !token) {
-    //   throw new Error('Authentication required');
-    // }
     return createContext();
   },
 });
